97f50e899096a1d245db43cac26d6df9b5b10956
Bara Sevcikova <bara.sevcikova@gmail.com>
2022-04-12 11:53:06 +0200

    vlozene pole

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
-const m = document.querySelector(".tlacitka");
+const m = document.querySelector(".policka");
 
-m.innerHTML = `<button>  </button>`;
+//m.innerHTML = `<button id="pole">  </button>`;
+
+const s = `<button id="pole">  </button>`;
+
+const pole = [
+  `${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
+  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}`,
+];
+
+m.innerHTML = pole;
