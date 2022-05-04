6993372ee94b72d87edaa3569dc22a9390bbec21
Bara Sevcikova <bara.sevcikova@gmail.com>
2022-05-04 21:59:31 +0200

    moje řešení úkolu 5

--- index.js
+++ index.js
@@ -1,74 +1,110 @@
-/*const m = document.querySelector(".policka");
-
-//m.innerHTML = `<button id="pole">  </button>`;
-
-const s = `<button id="pole">  </button>`;
-
-const pole = [
-  `${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}
-  ${s}${s}${s}${s}${s}${s}${s}${s}${s}${s}`,
-];
-
-m.innerHTML = pole;
-
-console.log("funguje");
-
-const btn = document.querySelector("button");
-
-btn.addEventListener("click", () => {
-  alert("baf");
-});
-*/
-const buttons = document.querySelectorAll(".pole");
-const players = ["circle", "cross"];
-const players_czech = ["kolečko", "křížek"];
-let a = 0;
-
-//
-
-const tlacitko = document.querySelectorAll("button");
 let player = "circle";
 
-tlacitko.forEach((btn) =>
-  btn.addEventListener("click", (event) => {
-    if (player == "circle") {
-      event.target.className = "board__field--circle";
-      btn.disabled = true;
-      player = "cross";
-      document.querySelector("#hraje").innerHTML =
-        'HRAJE: <img id="kolecko" src="podklady/2ukol/cross.svg" >';
-    } else {
-      event.target.className = "board__field--cross";
-      btn.disabled = true;
-      player = "circle";
-      document.querySelector("#hraje").innerHTML =
-        'HRAJE: <img id="kolecko" src="podklady/2ukol/circle.svg" "/>';
+const board = document.querySelectorAll(".wrapper--pole button");
+const hraje = document.querySelector(".hra--menu__hraje img");
+
+const handleClick = (event) => {
+  if (player === "circle") {
+    hraje.src = "podklady/2ukol/cross.svg";
+    player = "cross";
+    event.target.classList.add("board__field--circle");
+  } else {
+    player = "circle";
+    hraje.src = "podklady/2ukol/circle.svg";
+    event.target.classList.add("board__field--cross");
+  }
+  event.target.disabled = true;
+
+  const isWinning = isWinningMove(event.target);
+
+  if (isWinning) {
+    const symbol = getSymbol(event.target);
+    if (confirm(`Vyhrál ${symbol}. Spustit novou hru?`) === true) {
+      location.reload();
     }
-  })
-);
+  }
+  console.log(isWinningMove(event.target));
+};
+
+for (let i = 0; i < board.length; i++) {
+  board[i].addEventListener("click", handleClick);
+}
 
 const getSymbol = (field) => {
-  // Název třídy přizpůsob tvému kódu.
   if (field.classList.contains("board__field--cross")) {
-    return "cross";
+    return "křížek";
   } else if (field.classList.contains("board__field--circle")) {
-    return "circle";
+    return "kolečko";
   }
+  return undefined;
 };
 
-const boardSize = 10; // 10x10
-const fields = document.querySelectorAll(".pole"); // Selektor pozměň tak, aby odpovídal tvému kódu.
+const boardSize = 10;
+const fields = document.querySelectorAll(".wrapper--pole button");
+
 const getField = (row, column) => {
-  return buttons[row * fieldSize + column];
+  return fields[row * boardSize + column];
 };
 
-console.log(fields);
-//mám očíslovaná pole, potřebuji udělat event lisener na zmačknuté tlačítko
+const getPosition = (field) => {
+  let fieldIndex = 0;
+  while (fieldIndex < fields.length && field !== fields[fieldIndex]) {
+    fieldIndex++;
+  }
+
+  return {
+    row: Math.floor(fieldIndex / boardSize),
+    column: fieldIndex % boardSize,
+  };
+};
+
+const symbolsToWin = 5;
+const isWinningMove = (field) => {
+  const origin = getPosition(field);
+  const symbol = getSymbol(field);
+
+  let i;
+
+  let inRow = 1;
+  i = origin.column;
+  while (i > 0 && symbol === getSymbol(getField(origin.row, i - 1))) {
+    inRow++;
+    i--;
+  }
+
+  i = origin.column;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(origin.row, i + 1))
+  ) {
+    inRow++;
+    i++;
+  }
+
+  if (inRow >= symbolsToWin) {
+    return true;
+  }
+
+  let inColumn = 1;
+
+  i = origin.row;
+  while (i > 0 && symbol === getSymbol(getField(i - 1, origin.column))) {
+    inColumn++;
+    i--;
+  }
+
+  i = origin.row;
+  while (
+    i < boardSize - 1 &&
+    symbol === getSymbol(getField(i + 1, origin.column))
+  ) {
+    inColumn++;
+    i++;
+  }
+
+  if (inColumn >= symbolsToWin) {
+    return true;
+  }
+
+  return false;
+};
